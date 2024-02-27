61520d6cb621bceca4c1dc875072eeb1d83b470e
Rishal12 <rishalchand1@gmail.com>
2024-02-27 15:38:03 +1200

    Cal Age v1

--- calage.py
+++ calage.py
@@ -2,8 +2,6 @@ def calAGE():
     userName = input("Please Enter Your Name: ")
     bornRYear = int(input("Please Enter Your Born Year: "))
 
-    age = 2024 - bornRYear
-
     print("Hi," , userName)
     print("Your Age is: ", age)
 
